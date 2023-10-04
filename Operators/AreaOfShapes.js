bc474084d2e8fc52e9d4aafea80b42a9a2a421d3
qasachin <qasachin.chauhan@gmail.com>
2023-10-04 19:35:43 +0530

    renamed AreaOfShapes.js to areaOfShapes.js

--- Operators/AreaOfShapes.js
+++ Operators/AreaOfShapes.js
@@ -15,7 +15,6 @@ function areaOfTriangle(base, height){
     return (base * height) / 2;
 }
 
-
 console.log("area of the rectangle with length 12cm and bredth 6cm is : "+areaOfrectangle(12, 6));
 console.log("area of the square with side 8cm is "+areaOfSquare(8));
 console.log("area of the circle with the radius 13cm is :"+areaOfCircle(13));
